9c4e6ef7e43a451cf850b36dae474d98111de949
UnezaFatima <unezafatima74@gmail.com>
2024-11-08 17:10:12 -0500

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
 import "./App.css";
 // import Comp1 from "./components/literature/components/Comp1";
 // import {Demo1} from './components/navbar/Demo1';
-<<<<<<< HEAD
-import Footer from "./components/footer/Footer";
-=======
-// import { Demo } from "./components/home/Demo";
->>>>>>> 8ab4fcd10246e85301d5f0bc0a82e6cce38e5c28
+import Footer from './components/footer/Footer.jsx';
 
-function App() {
+function App(){
   return (
     <>
-<<<<<<< HEAD
       {/* <Demo1 />
       <Comp1 /> */}
       <Footer />
-=======
-      <h1 className="text-center text-2xl font-semibold p-8 bg-teal-100">
-        Welcome to E-Learning
-      </h1>
-      {/*  */}
-      {/* <Demo1 /> */}
-      {/* <Comp1 /> */}
-      {/* <Demo /> */}
->>>>>>> 8ab4fcd10246e85301d5f0bc0a82e6cce38e5c28
     </>
   );
 }
