def0d6c7b8922306a5e662c87303071db6db9274
UnezaFatima <unezafatima74@gmail.com>
2024-11-08 17:03:49 -0500

    Footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import "./App.css";
-import Comp1 from "./components/literature/components/Comp1";
-import {Demo1} from './components/navbar/Demo1';
+// import Comp1 from "./components/literature/components/Comp1";
+// import {Demo1} from './components/navbar/Demo1';
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
-      <Demo1 />
-      <Comp1 />
+      {/* <Demo1 />
+      <Comp1 /> */}
+      <Footer />
     </>
   );
 }
