32730364847b807ef850566c161be96c0df8c0c1
UnezaFatima <unezafatima74@gmail.com>
2024-11-08 00:34:27 -0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import "./App.css";
+import Comp1 from "./components/literature/components/Comp1";
 import {Demo1} from './components/navbar/Demo1';
 
 function App() {
   return (
     <>
-     
       <Demo1 />
+      <Comp1 />
     </>
   );
 }
---
+++ src/components/literature/demo.jsx
@@ -0,0 +1,7 @@
+import Comp1 from "./components/Comp1"
+
+export const Demo1 = () => {
+    return <div>
+      <Comp1/>
+    </div>
+}
