259746862fe23b1fffaf6ba0ec3201348847b916
UnezaFatima <unezafatima74@gmail.com>
2024-11-07 21:18:59 -0500

    done

--- src/components/navbar/demo.jsx
+++ src/components/navbar/demo.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
 import Nav1 from './components/Nav1.jsx';
+import Nav2 from './components/Nav2.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
