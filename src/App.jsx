862114431a099d4ce1098c04a82a4a08cb7a692b
impcakash <impcakash@gmail.com>
2024-11-08 11:35:58 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
 import "./App.css";
-import Comp1 from "./components/literature/components/Comp1";
-import {Demo1} from './components/navbar/Demo1';
+// import Comp1 from "./components/literature/components/Comp1";
+// import {Demo1} from './components/navbar/Demo1';
+// import { Demo } from "./components/home/Demo";
 
 function App() {
   return (
     <>
-      <Demo1 />
-      <Comp1 />
+      {/* <Demo1 /> */}
+      {/* <Comp1 /> */}
+      {/* <Demo /> */}
     </>
   );
 }
