54940d659fb1e203127d98c47177cbd8e2c21607
Manish Sahu <101319723+Manishsahu116@users.noreply.github.com>
2024-11-12 18:22:04 +0530

    Update Membership.jsx

--- src/components/membership/Membership.jsx
+++ src/components/membership/Membership.jsx
@@ -1,1 +1,23 @@
+import React from 'react';
+import PricingSection from './components/PricingSection';
+import OnlineCoaching from './components/OnlineCoaching';
+import CoachingLessons from './components/CoachingLessons';
+import StudentsHaveTo from './components/StudentsHaveTo';
+import DownloadBanner from './components/DownloadBanner';
+import RoleCard from './components/RoleCard';
+
+function Membership() {
+  return (
+    <div className="Membership">
+      <PricingSection />
+      <OnlineCoaching />
+      <CoachingLessons />
+      <StudentsHaveTo />
+      <DownloadBanner />
+      <RoleCard />
+    </div>
+  );
+}
+
+export default App;
 
