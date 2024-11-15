29579039db28455fe8b796e8de1706e60d0bec60
impcakash <impcakash@gmail.com>
2024-11-15 21:02:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,14 @@
 import "./App.css";
 import Navbar from "./components/navbar/components/Nav1.jsx";
 import Footer from "./components/footer/Footer.jsx";
-import Login from "./components/navbar/components/Login.jsx";
-import Meeting from "./components/other_section/Meeting/Meeting";
-import BlogDetail from "./components/other_section/Blog_Detail/BlogDet.jsx";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
-      {/* <Navbar2 /> */}
-      {/* <Navbar2 />
-      <Component1 />
-      <Component2 />
-      <Component3 />
-      <Footer/>
-      <Login/> */}
-      {/* <Meeting /> */}
-      {/* <BlogDetail /> */}
-
-      {/* <Homepage /> */}
-
-      {/*-------------- MEDHAVI ------------------------------ */}
-
-      {/* BLOGG PAGEE */}
-      {/* <Blog/> */}
-
-      {/* OTHERS=> CHECKOUT PAGE */}
-      {/* <CheckoutMain/> */}
-
-      {/* OTHERS=> COURSE_DETAILS2 PAGE */}
-      {/* <Course_Details/> */}
-
-      {/* ---------------------------------------------------- */}
-      {/* <CourseCalender /> */}
-
-      {/* <Calendar_01 /> */}
-      {/* <Calendar_02 /> */}
-
-      {/* viqar=> course page */}
-      {/* <Courses_nav /> */}
-
-      {/*  */}
-      {/* <Footer /> */}
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
   );
 }
