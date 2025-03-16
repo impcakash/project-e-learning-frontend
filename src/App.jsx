fd35583ecaa170aee30abfea10a811a935ec2f19
impcakash <impcakash@gmail.com>
2025-03-16 22:13:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import Navbar from "./components/navbar/components/Nav1.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import { Outlet } from "react-router-dom";
 
-
 function App() {
   return (
     <>
       <Navbar />
       <Outlet />
       <Footer />
-  
     </>
   );
 }
